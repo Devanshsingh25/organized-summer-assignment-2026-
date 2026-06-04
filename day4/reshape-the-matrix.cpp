8e6bb0a888b7037de452d7db200a68154c0529e4
Devanshsingh25 <nikk501101@gmail.com>
2026-06-04 16:51:41 +0530

    Create reshape-the-matrix.cpp

---
+++ day4/reshape-the-matrix.cpp
@@ -0,0 +1,26 @@
+//reshape the matrix
+
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        vector<vector<int>>ans(r,vector<int>(c));
+        int m = mat.size();
+        int n = mat[0].size();
+        int row = 0;
+        int col = 0;
+        if(m*n!=r*c)
+        return mat;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                ans[row][col]=mat[i][j];
+                col++;
+                if(col == c){
+                    row++;
+                    col =0;
+                }
+            }
+
+        }
+      return ans;  
+    }
+};
